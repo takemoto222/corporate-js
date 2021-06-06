180f2aeb4f51ad25460043c546c3fb2d89d41626
takemoto222 <omochi9797@icloud.com>
2021-06-06 19:38:14 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//hamburge
+//hamburger
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  const target = document.getElementById("menu");
+  target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
+});
+//hamburgerここまで
+
+//hamburgerここまで
+
+
+
+
