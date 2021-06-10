89b3a1230b4860ecd23d98cc69314bf8f250a8e4
takemoto222 <omochi9797@icloud.com>
2021-06-10 15:16:14 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -14,3 +14,14 @@ target.addEventListener('click', () => {
 
 
 
+function scrollTop(elem) {
+  const target = document.getElementById(elem);
+  target.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
+
+scrollTop('button');
